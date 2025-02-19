e39c8d0f5440f346b64a39e5a5e97d61b7268dee
fengtiecheng <17615007230@163.com>
2025-02-19 17:22:37 +0800

    style(启动类): 修改启动类描述
    1.修改启动类描述

--- deepseek/src/main/java/com/ftc/deepseek/DeepSeekApplication.java
+++ deepseek/src/main/java/com/ftc/deepseek/DeepSeekApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author 17615007230@163.com
+ * @author: 冯铁城 [17615007230@163.com]
+ * @date: 2025-02-19 14:26:37
+ * @describe: 项目启动类
  */
 @SpringBootApplication
-public class DeepseekApplication {
+public class DeepSeekApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DeepseekApplication.class, args);
+        SpringApplication.run(DeepSeekApplication.class, args);
     }
 
 }
